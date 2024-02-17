b3b5ab20724e811d9463c701f53a8dcef5b497dc
VictorEyeEagle <victor100bol@hotmail.com>
2024-02-17 20:04:31 -0300

    Melhora no codigo

--- cicd-build.sh
+++ cicd-build.sh
@@ -1,19 +1,31 @@
 #!/bin/bash -xe
-APP=$1
-ROOT=$(pwd)
 
-cd "$APP"
+APP_NAME=$1
+ROOT_DIRECTORY=$(pwd)
+
+if [ -z "$APP_NAME" ]; then
+  echo "Erro: Argumento insuficiente fornecido."
+  echo "Uso: $0 <nome_da_aplicacao>"
+  exit 1
+fi
+
+cd "$APP_NAME"
 
 ./mvnw clean
+
 ./mvnw versions:set -DremoveSnapshot
+
 APP_VERSION=$(./mvnw -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
+
 ./mvnw package
+
 ./mvnw versions:set -DnextSnapshot
 
 git add pom.xml
-git commit -m "cicd: bump version ${APP}:${APP_VERSION}"
+git commit -m "cicd: bump version ${APP_NAME}:${APP_VERSION}"
 
-cd "$ROOT"
-TAG=$APP_VERSION docker compose build --no-cache "$APP"
+cd "$ROOT_DIRECTORY"
 
-docker images "dio/${APP}"
+TAG=$APP_VERSION docker compose build --no-cache "$APP_NAME"
+
+docker images "dio/${APP_NAME}"
