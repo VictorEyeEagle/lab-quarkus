a52a57aa040c02cb9a67d4353b64e4aa6107343f
VictorEyeEagle <victor100bol@hotmail.com>
2024-02-17 19:55:29 -0300

    Melhora no codigo

--- cicd-blue-green-deployment.sh
+++ cicd-blue-green-deployment.sh
@@ -1,25 +1,28 @@
 #!/bin/bash -xe
-APP=$1
-GREEN_CONTAINER_DEPLOY_TAG=$2
+
+APP_NAME=$1
+GREEN_CONTAINER_TAG=$2
 
 if [ $# -lt 2 ]; then
-  exit
+  echo "Erro: Argumentos insuficientes fornecidos."
+  echo "Uso: $0 <nome_da_aplicacao> <tag_do_container_verde>"
+  exit 1
 fi
 
-BLUE_CONTAINERS=$(docker ps -qf "name=${APP}")
-BLUE_CONTAINERS_SCALE=$(echo "$BLUE_CONTAINERS" | wc -w | xargs)
+BLUE_CONTAINERS=$(docker ps -qf "name=${APP_NAME}")
+BLUE_CONTAINER_COUNT=$(echo "$BLUE_CONTAINERS" | wc -w | xargs)
 
-GREEN_CONTAINERS_SCALE=$((BLUE_CONTAINERS_SCALE * 2))
-if [[ $BLUE_CONTAINERS_SCALE == 0 ]]; then
-    GREEN_CONTAINERS_SCALE=1
+GREEN_CONTAINER_COUNT=$((BLUE_CONTAINER_COUNT * 2))
+if [[ $BLUE_CONTAINER_COUNT == 0 ]]; then
+    GREEN_CONTAINER_COUNT=1
 fi
 
-TAG=$GREEN_CONTAINER_DEPLOY_TAG docker compose up -d "$APP" --scale "$APP=$GREEN_CONTAINERS_SCALE" --no-recreate --no-build
+TAG=$GREEN_CONTAINER_TAG docker compose up -d "$APP_NAME" --scale "$APP_NAME=$GREEN_CONTAINER_COUNT" --no-recreate --no-build
 
-until [ "$(docker ps -q -f "name=${APP}" -f "health=healthy" | wc -l | xargs)" == $GREEN_CONTAINERS_SCALE ]; do
+until [ "$(docker ps -q -f "name=${APP_NAME}" -f "health=healthy" | wc -l | xargs)" == $GREEN_CONTAINER_COUNT ]; do
     sleep 0.1;
 done;
 
-if [[ $BLUE_CONTAINERS_SCALE -gt 0 ]]; then
+if [[ $BLUE_CONTAINER_COUNT -gt 0 ]]; then
     docker kill --signal=SIGTERM "$BLUE_CONTAINERS"
 fi
